7f25cb571ce8c88c5a7561c7ef2a21897d4380f7
PawanMahesh-V <pawanmahesh94@gmail.com>
2026-01-02 23:32:42 +0500

    Modified Some Files

--- frontend/src/pages/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage.jsx
@@ -1,6 +1,6 @@
 import { authApi } from '../api';
 import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faLock, 
